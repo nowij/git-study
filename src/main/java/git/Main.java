2dca1e6f3f9c9f60c4c2a040d6e7c9bea9e15b1a
nowij <nowijdl@gmail.com>
2022-07-01 11:01:47 +0900

    내용 추가

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -3,8 +3,8 @@ package main.java.git;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("내용1");
+		System.out.println("내용2");
 	}
 
 }
